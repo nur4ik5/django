f0c818932624395d47512c6d2b1659066f739e54
Nurjigit <sagendikov45@gmail.com>
2021-12-27 19:59:47 +0600

    В posts добавил картинку

--- posts/models.py
+++ posts/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Post(models.Model):
 	title = models.CharField(max_length = 30, verbose_name='Заголовок')
 	text = models.TextField(verbose_name='Статья')
-	cover = models.ImageField(upload_to='post_covers/', null=True, blank=True)
+	cover = models.ImageField(upload_to='post_covers/', null=True, blank=True, default='post_covers/default.png')
 
 	def __str__(self):
 		return self.title
